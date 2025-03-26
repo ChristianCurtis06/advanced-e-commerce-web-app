2b42fe6a43c4d79402ce41ef87d78e19796735ca
ChristianCurtis06 <ctcurtis10@gmail.com>
2025-03-26 09:34:50 -0500

    Updated Mini-Project

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,17 @@
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Container, Navbar, Nav, Badge } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
 const NavBar: React.FC = () => {
+  const cart = useSelector((state: RootState) => state.cart.cart);
+  const [productCount, setProductCount] = useState<number>(0);
+
+  useEffect(() => {
+    const countTotal = cart.reduce((acc, product) => acc + (product.quantity || 1), 0);
+    setProductCount(countTotal);
+  }, [cart]);
+
   return (
     <Navbar bg="warning" expand="lg" variant="dark">
       <Container fluid>
@@ -9,7 +20,7 @@ const NavBar: React.FC = () => {
         <Navbar.Collapse id="navbarNav">
           <Nav className="ms-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/cart">Cart</Nav.Link>
+            <Nav.Link href="/cart">Cart<Badge className="ms-1 bg-white text-dark">{productCount}</Badge></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
